a00e87e7fb42251cdd6ee0ae08dccac982f99962
Tatan <tatangamess@gmail.com>
2026-07-30 08:44:41 -0600

    mantenimiento

--- app/Http/Controllers/Login/LoginController.php
+++ app/Http/Controllers/Login/LoginController.php
@@ -16,8 +16,7 @@ class LoginController extends Controller
 
     public function index(){
 
-        return view('mantenimiento');
-        //return view('frontend.login.vistalogin');
+        return view('frontend.login.vistalogin');
     }
 
     public function login(Request $request){
